040a2d21858283bb1fca00c6972f7490cb85cdff
Owais Yosuf <johnowais@gmail.com>
2025-04-25 11:11:43 +0530

    Validation using express-validator

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 // Create a new user
-router.post('/register', (req, res) => {
-    console.log(req.body);
-    const user = User(req.body);
-    user.save()
-    res.send("User registered successfully");
+router.post('/register', [
+    body('name', "Name can't be less than 3 characters").isLength({ min: 3 }),
+    body('email', "Invalid email").isEmail(),
+    body('password', "Password can't be less than 8 characters").isLength({ min: 8 }),
+], (req, res) => {
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+        User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        }).then(user => res.json(user))
+            .catch(err => res.status(500).json({ error: err.message }));
+    } else {
+        res.send({ errors: result.array() })
+    };
 })
 
 router.get('/', (req, res) => {
