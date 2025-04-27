52af23ebd29efebcde4591d9cffe79df79356441
Owais Yosuf <johnowais@gmail.com>
2025-04-27 12:31:07 +0530

    Password hashing using bcryptjs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,23 +2,36 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = "MyNameIsOwaisAndIAmTheBestDeveloperInTheWorld";
 
 // Create a new user
 router.post('/register', [
     body('name', "Name can't be less than 3 characters").isLength({ min: 3 }),
     body('email', "Invalid email").isEmail(),
     body('password', "Password can't be less than 8 characters").isLength({ min: 8 }),
-], (req, res) => {
+], async (req, res) => {
     const result = validationResult(req);
     if (result.isEmpty()) {
-        User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
-        }).then(user => res.json(user))
-            .catch(err => res.status(500).json({ error: err.message }));
-    } else {
-        res.send({ errors: result.array() })
+        try {
+            let user = await User.findOne({ email: req.body.email });
+            if (user) {
+                return res.status(400).json({ error: "User already exists" });
+            }
+            const salt = bcrypt.genSaltSync(10);
+            const securedPassword = bcrypt.hashSync(req.body.password, salt);
+            user = await User.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: securedPassword
+            })
+            const authToken = jwt.sign({ id: user.id }, JWT_SECRET);
+            res.json({ authToken });
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Internal Server Error");
+        }
     };
 })
 
